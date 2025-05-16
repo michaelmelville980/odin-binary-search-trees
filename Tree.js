f8d260e2f9ebc636a6f36478093ef0fff5ff029f
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-16 11:40:16 -0400

    add buildTree method implementation draft

--- Tree.js
+++ Tree.js
@@ -7,8 +7,28 @@ export default class Tree {
         this.root = buildTree();
     }
 
-    buildTree(array){
-        
+    buildTree(array = []){
+
+        /* Sorting and Filtering Duplicates */
+        array.sort((a, b) => a - b); 
+        array = array.filter((item, index, arr) =>
+            arr.indexOf(item) === index 
+        );
+
+        /* Initializing start, end and making sure |tree| !=== 0 (base case) */
+        let start = array[0];
+        let end = array[array.length - 1];
+        if(start > end) return null;
+
+        /* Creating Tree Node*/
+        let mid = Math.floor((start + end) / 2);
+        let root = new Node(
+            array[mid], 
+            buildTree(array.slice(start, middle-1)), //Building left subtree
+            buildTree(array.slice(middle + 1, end)) //Building right subtree
+        );
+        return root;
+
     }
     
 }
