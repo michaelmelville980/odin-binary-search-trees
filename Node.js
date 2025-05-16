40439c54a0b4a3be273b3d1b97c9486940cb180b
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-16 13:05:38 -0400

    refactoring insert(value) and adding it to Node class instead of Tree class

--- Node.js
+++ Node.js
@@ -7,4 +7,27 @@ export default class Node{
         this.right = right;
     }
 
+    insert(value){
+
+    if(value === this.data) return;
+
+    if(!this.data){
+        this.root = new Node(value);
+    }else{
+        if(value < this.data){
+            if(this.left){
+                this.left.insert(value);
+            }else{
+                this.left = new Node(value);
+            }
+        }else if (value > this.data){
+            if(this.right){
+                this.right.insert(value);
+            }else{
+                this.right = new Node(value);
+            }
+        }
+    }
 }
+}
+    
--- Tree.js
+++ Tree.js
@@ -30,46 +30,6 @@ export default class Tree {
         );
     }
 
-    insert(value){
-
-        /* Node with no children */
-        if (this.left === null && this.right === null){
-            if (value < this.data){
-                this.left = new Node(value);
-            }else if (value > this.data){
-                this.right = new Node(value);
-            }
-        }
-
-        /* Node with only left child*/
-        else if (this.left !== null && this.right === null){
-            if (value < this.data){
-                this.left.insert(value);
-            }else if (value > this.data){
-                this.right = new Node(value)
-            }
-        }
-
-        /* Node with only right child */
-        else if (this.left === null && this.right !== null){
-            if (value < this.data){
-                this.left = new Node(value);
-            }else if (value > this.data){
-                this.right.insert(value);
-            }
-        }
-
-        /* Node with both children */
-        else {
-            if (value < this.data){
-                this.left = this.left.insert(value);
-            }else if (value > this.data){
-                this.right = this.right.insert(value);
-            }
-        }
-
-    }
-
 
     deleteItem(value){
 
