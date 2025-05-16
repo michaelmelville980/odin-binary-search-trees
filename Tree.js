feb8c128d19d307702f78bc8db28b2a050a55d89
michael1303 <88162384+michael1303@users.noreply.github.com>
2025-05-16 12:03:02 -0400

    -preparing array (sort + filter duplicates) outside of buildTree
    -changing start/end to reference indices instead of array elements
    
    -properly using array.slice in return call

--- Tree.js
+++ Tree.js
@@ -2,33 +2,32 @@ import Node from './Node.js';
 
 export default class Tree {
 
-    constructor(array = null){
-        this.array = array;
-        this.root = buildTree();
+    constructor(array = []){
+        this.array = this.prepArray(array);
+        this.root = this.buildTree(this.array);
     }
 
+    /* Sorts and Filters Duplicates from Array*/
+    prepArray(array = []){
+        array.sort((a, b) => a - b); 
+        array = array.filter((item, index, arr) => arr.indexOf(item) === index );
+        return array;
+    }
+
+    /* Builds Tree and Returns Root Node */
     buildTree(array = []){
 
-        /* Sorting and Filtering Duplicates */
-        array.sort((a, b) => a - b); 
-        array = array.filter((item, index, arr) =>
-            arr.indexOf(item) === index 
-        );
+        if(array.length === 0) return null;
 
-        /* Initializing start, end and making sure |tree| !=== 0 (base case) */
-        let start = array[0];
-        let end = array[array.length - 1];
-        if(start > end) return null;
-
-        /* Creating Tree Node*/
+        let start = 0;
+        let end = array.length;
         let mid = Math.floor((start + end) / 2);
-        let root = new Node(
-            array[mid], 
-            buildTree(array.slice(start, middle-1)), //Building left subtree
-            buildTree(array.slice(middle + 1, end)) //Building right subtree
-        );
-        return root;
 
+        return new Node(
+            array[mid],
+            this.buildTree(array.slice(start, mid)),
+            this.buildTree(array.slice(mid + 1, end)),
+        );
     }
-    
+
 }
